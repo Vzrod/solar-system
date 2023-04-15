aee47bfa310bb8bede09b241bde9f74de64740ca
Vzrod <arthur.clot@icloud.com>
2023-04-15 14:50:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Mar 19 13:40:19 2023
-
 @author: arthu
 """
 import csv
@@ -25,11 +24,10 @@ for i in range(len(data)):
     distances[i]['x'] = float(data[i]['distance'])
     vitesses[i]['y'] = float(data[i]['vitesse'])
     data[i]['masse'] = float(data[i]['masse'])
-    
+
 while t<10*365*sec_jour:
     for astre1 in range(len(data)-1):
         for astre2 in range(astre1+1, len(data)):
             dx, dy, dz = distances[astre1]['x'] - distances[astre2]['x'], distances[astre1]['y'] - distances[astre2]['y'], distances[astre1]['z'] - distances[astre2]['z']
             d = (dx**2+dy**2+dz**2)
-            
-            
+

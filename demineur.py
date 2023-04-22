d5c9f2f8c685dabadbfbad429738df9b5c98711a
Vzrod <arthur.clot@icloud.com>
2023-04-22 14:53:29 +0200

    Ajout fonc affichage console

--- demineur.py
+++ demineur.py
@@ -25,7 +25,7 @@ def gen_mines(dico: dict, nbbombes):
         nb = liste.pop()
         dico[(nb//(xmax), nb%ymax)][2] = -1
         listeb.append((nb//(xmax), nb%ymax))
-    
+   
     return dico, listeb
 
 def aff_nb(dico: dict, listeb):
@@ -37,38 +37,25 @@ def aff_nb(dico: dict, listeb):
         for i in range(9):
             if (((i//3)+coinX, (i%3)+coinY) in dico) and not(i==4):
                 dico[((i//3)+coinX, (i%3)+coinY)][3]+=1
-            
+           
     return dico
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def affichage(dico: dict):
+    xmax = max([key[0] for key in dico.keys()])+1
+    ymax = max([key[1] for key in dico.keys()])+1
+    for i in range(xmax):
+        for y in range(ymax):
+            if dico[(i,y)][2]!=-1:
+                print(f' {dico[(i,y)][3]} |', end='')
+            else :
+                print(' X |', end='')
+        print('')
+       
+       
+a = gen_dico((3,3))
+b, nb = gen_mines(a, 1)
+c = aff_nb(b, nb)
+affichage(c)
 
 
 
@@ -79,6 +66,6 @@ def gen_liste(taille: tuple):
     liste = [[[[0], [0], [0], [0]] for _ in range(taille[0])] for _ in range(taille[1])]
     return liste
 
-print('Dico', getsizeof(gen_dico((500,500))))
-print('Liste', getsizeof(gen_liste((500,500))))
+print('Dico', getsizeof(gen_dico((100,100))))
+print('Liste', getsizeof(gen_liste((100,100))))
 """
